a6a0ea991bab15d98f90942d1d543b5b1e23dd12
Stephen McLaughlin <steve.mclaughlin@medicinalgenomics.com>
2026-01-09 18:48:44 +0000

    shell scriptthat calls plot_newick_highlight-v3.R with the appropriate arguments to create highlighted newick trees

---
+++ Mutant_Cannabis/scripts/call-v3-tree-kannapedia-tree-public-only.sh
@@ -0,0 +1,7 @@
+./plot_newick_highlight-v3.R \
+  -n kannapedia-tree-public-samples-only.newick \
+  -o kannapedia-tree-public-samples-only \
+  -H RSP13489:"Opalo" RSP13487:"Violetta Swag" RSP13488:"Viho Tinto" RSP11211:"80E #1" RSP12876:"Swag Trinidat, by Kaly"  RSP11212:"80E #2" RSP11213:"80E #3" \
+  --layout circular \
+  --combine
+
